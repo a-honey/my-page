0f2fd0ab7715ff7c114ca3d4a1e5a3b3ab784408
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-01 19:10:17 +0900

    Created Education type

--- client/components/main/Education.tsx
+++ client/components/main/Education.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { Education } from "@/types/mainType";
+
 const Education = () => {
-  return <div>Education</div>;
+  const [education, setEducation] = useState<Education[]>([]);
+
+  const getExperiences = async () => {
+    await fetch("../../public/data/education.json")
+      .then((res) => res.json())
+      .then((data) => setEducation(data));
+  };
+
+  useEffect(() => {
+    try {
+      getExperiences();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <>
+      <div>Education</div>
+      {education}
+    </>
+  );
 };
 
 export default Education;
---
+++ client/types/mainType.d.ts
@@ -0,0 +1,8 @@
+export declare module "mainType";
+
+type Education = {
+  _id: number;
+  name: string;
+  period: string;
+  explanation?: string;
+};
