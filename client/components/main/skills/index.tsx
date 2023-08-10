803876feef8096f6051b5687ce066a1d3c3c6557
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-10 23:04:23 +0900

    [ADD] skills svg 추가 및 그리드 배열

--- client/components/main/skills/index.tsx
+++ client/components/main/skills/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import * as Type from "../../../types/mainType";
 import styles from "./skills.module.sass";
 
@@ -6,41 +7,38 @@ const Skills = ({ skills }: { skills: Type.Skills | null }) => {
     <div className="main-container" id="skills">
       <h1 className="main-name">skills</h1>
       <div className={styles.contentContainer}>
-        <div className={styles.itemContainer}>
+        <div className={`${styles.itemContainer} ${styles.front}`}>
           <h2>Frontend</h2>
-          <ul>
-            {skills?.frontend.map((skill) => (
-              <li>{skill.name}</li>
-            ))}
-          </ul>
+          <Image
+            src="/assets/fronts.svg"
+            alt="front"
+            width={240}
+            height={600}
+          />
         </div>
-        <div className={styles.itemContainer}>
+        <div className={`${styles.itemContainer} ${styles.back}`}>
           <h2>Backend</h2>
-          <ul>
-            {skills?.backend.map((skill) => (
-              <li>{skill.name}</li>
-            ))}
-          </ul>
+          <Image src="/assets/backs.svg" alt="front" width={240} height={600} />
         </div>
-        <div className={styles.itemContainer}>
-          <h2>Version Control</h2>
-          <ul>
-            {skills?.versionControl.map((skill) => (
-              <li>{skill.name}</li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles.itemContainer}>
+        <div className={`${styles.itemContainer} ${styles.communication}`}>
           <h2>Communication</h2>
-          <ul>
-            {skills?.communication ? (
-              skills.communication.map((skill) => <li>{skill.name}</li>)
-            ) : (
-              <li>협업도구 없음</li>
-            )}
-          </ul>
+          <Image
+            src="/assets/developments.svg"
+            alt="front"
+            width={240}
+            height={600}
+          />
         </div>
-        <div className={styles.itemContainer}>
+        <div className={`${styles.itemContainer} ${styles.version}`}>
+          <h2>Version Control</h2>
+          <Image
+            src="/assets/version.svg"
+            alt="front"
+            width={240}
+            height={600}
+          />
+        </div>
+        <div className={`${styles.itemContainer} ${styles.certificate}`}>
           <h2>Certificate</h2>
           <ul>
             {skills?.certificate ? (
