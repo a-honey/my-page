f009b105904a6dc52a20c04fb88aa800870cb69b
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-10 19:06:35 +0900

    [PAUSE] getStaticProps json fetch 오류

--- client/containers/Main.tsx
+++ client/containers/Main.tsx
@@ -1,52 +1,28 @@
 import Projects from "@/components/main/projects";
 import About from "@/components/main/about";
 import Skills from "@/components/main/skills";
-import Footer from "@/components/common/Footer";
-import * as Type from "../types/mainType";
-import { useEffect, useState } from "react";
 import Contact from "@/components/main/contact/Contact";
 
-type Props = {};
+import * as Type from "../types/mainType";
 
-const Main = ({}: Props) => {
-  const [isFetching, setIsFetching] = useState(false);
-  const [intro, setIntro] = useState<Type.Intro | null>(null);
-  const [about, setAbout] = useState<Type.About | null>(null);
-  const [skills, setSkills] = useState<Type.Skills | null>(null);
-  const [projects, setProjects] = useState<Type.Projects | null>(null);
+export type DataProps = {
+  intro: Type.Intro | null;
+  about: Type.About | null;
+  skills: Type.Skills | null;
+  projects: Type.Projects | null;
+};
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsFetching(true);
-        const res = await fetch("/data/main.json");
-        const data = await res.json();
-        console.log("나는 콘솔");
-        console.log(data);
-        setIntro(data.intro);
-        setAbout(data.about);
-        setProjects(data.projects);
-        setSkills(data.skills);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setIsFetching(false);
-      }
-    }
-    fetchData();
-  }, []);
-
-  if (isFetching) {
-    return <div>Loading...</div>;
+const Main = ({ data }: { data: DataProps }) => {
+  if (!data) {
+    return <div>Error loading data.</div>;
   }
-
   return (
     <div
       className={`snap-y snap-mandatory scroll-smooth h-screen overflow-y-auto`}
     >
-      <About about={about} intro={intro} />
-      <Skills skills={skills} />
-      <Projects projects={projects} />
+      <About about={data.about} intro={data.intro} />
+      <Skills skills={data.skills} />
+      <Projects projects={data.projects} />
       <Contact />
     </div>
   );
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,52 @@
-import Main from "@/containers/Main";
+import Main, { DataProps } from "@/containers/Main";
+import { InferGetServerSidePropsType, GetStaticProps } from "next";
 import React from "react";
+import path from "path";
 
-const MainPage: React.FC = () => {
-  return (
-    <Main/>
-  );
+type Props = {
+  data: DataProps[];
+};
+
+const MainPage = async ({
+  data,
+}: InferGetServerSidePropsType<typeof getStaticProps>) => {
+  console.log(data);
+  const filePath = path.join(process.cwd(), "data", "main.json");
+  const res = await fetch(filePath);
+  console.log(filePath);
+  console.log(res.json());
+  return <Main data={data} />;
 };
 
 export default MainPage;
+
+export const getStaticProps: GetStaticProps<{
+  data: DataProps;
+}> = async () => {
+  console.log("dd");
+  const filePath = path.join(process.cwd(), "data", "main.json");
+  console.log("ddd");
+  try {
+    console.log("d");
+    const res = await fetch(filePath);
+    console.log(res.status);
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data. Status: ${res.status}`);
+    }
+
+    const data = await res.json();
+
+    if (!data) {
+      return { notFound: true };
+    }
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { props: { data: null } };
+  }
+};
