c3d2aa03b0ce62f882dbc2c81c35a0939c7fd5db
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-10 15:01:40 +0900

    [ADD] 모바일 반응형 추가

--- client/containers/Main.tsx
+++ client/containers/Main.tsx
@@ -9,7 +9,7 @@ import Contact from "@/components/main/contact/Contact";
 type Props = {};
 
 const Main = ({}: Props) => {
-  const [ isFetching, setIsFetching ] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
   const [intro, setIntro] = useState<Type.Intro | null>(null);
   const [about, setAbout] = useState<Type.About | null>(null);
   const [skills, setSkills] = useState<Type.Skills | null>(null);
@@ -21,31 +21,32 @@ const Main = ({}: Props) => {
         setIsFetching(true);
         const res = await fetch("/data/main.json");
         const data = await res.json();
-        console.log('나는 콘솔');
+        console.log("나는 콘솔");
         console.log(data);
         setIntro(data.intro);
         setAbout(data.about);
         setProjects(data.projects);
-        setSkills(data.skills); 
+        setSkills(data.skills);
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
         setIsFetching(false);
       }
     }
-
     fetchData();
-  },[])
+  }, []);
 
-  if (!isFetching) {
-    <div>Loading...</div>;
+  if (isFetching) {
+    return <div>Loading...</div>;
   }
 
   return (
-    <div className="h-screen snap-y snap-mandatory scroll-smooth overflow-y-auto">
+    <div
+      className={`snap-y snap-mandatory scroll-smooth h-screen overflow-y-auto`}
+    >
       <About about={about} intro={intro} />
-      <Skills skills={skills}/>
-      <Projects projects={projects}/>
+      <Skills skills={skills} />
+      <Projects projects={projects} />
       <Contact />
     </div>
   );
