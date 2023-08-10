6d2a9aaa7ef13d7c3545d522a699fb5da2910b26
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-10 20:46:42 +0900

    [ADD] getStaticProps js형식으로 추가

--- client/containers/Main.tsx
+++ client/containers/Main.tsx
@@ -2,51 +2,26 @@ import Projects from "@/components/main/projects";
 import About from "@/components/main/about";
 import Skills from "@/components/main/skills";
 import Footer from "@/components/common/Footer";
+import { data } from "../public/data/main";
 import * as Type from "../types/mainType";
 import { useEffect, useState } from "react";
 import Contact from "@/components/main/contact/Contact";
 
-type Props = {};
+export type Props = {
+  about: Type.About;
+  intro: Type.Intro;
+  skills: Type.Skills;
+  projects: Type.Projects;
+};
 
-const Main = ({}: Props) => {
-  const [isFetching, setIsFetching] = useState(false);
-  const [intro, setIntro] = useState<Type.Intro | null>(null);
-  const [about, setAbout] = useState<Type.About | null>(null);
-  const [skills, setSkills] = useState<Type.Skills | null>(null);
-  const [projects, setProjects] = useState<Type.Projects | null>(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsFetching(true);
-        const res = await fetch("/data/main.json");
-        const data = await res.json();
-        console.log("나는 콘솔");
-        console.log(data);
-        setIntro(data.intro);
-        setAbout(data.about);
-        setProjects(data.projects);
-        setSkills(data.skills);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setIsFetching(false);
-      }
-    }
-    fetchData();
-  }, []);
-
-  if (isFetching) {
-    return <div>Loading...</div>;
-  }
-
+const Main = ({ importData }: { importData: Props }) => {
   return (
     <div
       className={`snap-y snap-mandatory scroll-smooth h-screen overflow-y-auto`}
     >
-      <About about={about} intro={intro} />
-      <Skills skills={skills} />
-      <Projects projects={projects} />
+      <About about={importData.about} intro={importData.intro} />
+      <Skills skills={importData.skills} />
+      <Projects projects={importData.projects} />
       <Contact />
     </div>
   );
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,18 @@
-import Main from "@/containers/Main";
+import Main, { Props } from "@/containers/Main";
+import { data } from "@/public/data/main";
 import React from "react";
+import { GetStaticProps } from "next";
 
-const MainPage: React.FC = () => {
-  return (
-    <Main/>
-  );
+const MainPage = ({ importData }: { importData: Props }) => {
+  return <Main importData={importData} />;
 };
 
 export default MainPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      importData: data,
+    },
+  };
+};
