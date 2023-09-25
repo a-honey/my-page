698c87d4cf6d4c54e371193eefc691026daab80d
a-honey <jungahyeon0512@khu.ac.kr>
2023-09-25 23:22:49 +0900

    :lipstick: Design: project 페이지 구성 셋팅

--- client/components/Projects/Project.tsx
+++ client/components/Projects/Project.tsx
@@ -1,42 +1,46 @@
 import * as Type from "../../types/mainType";
+import Header from "../common/Header";
 import styles from "./project.module.scss";
 import Image from "next/image";
 
 const Project = ({ project }: { project: Type.Project }) => {
   return (
     <main className="bright">
-      <div className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="/assets/image.png"
-            width={800}
-            height={600}
-            alt="project"
-          />
-        </div>
-        <div className={styles.contentContainer}>
-          <h1>{project.title}</h1>
-          <h2>{project.subTitle}</h2>
-          <div className={styles.container}>
-            <div className={styles.textContainer}>
-              <p>{project.description}</p>
-              <ul>
-                <li>
-                  <label>주요 기능</label>
-                  <div>{project.featuresDescription}</div>
-                </li>
-                <li>
-                  <label>역할</label>
-                  <div>{project.role}</div>
-                </li>
-                <li>
-                  <label>스택</label>
-                  <div>{project.stack}</div>
-                </li>
-                <li>
-                  <a href={project.projectRepository}>프로젝트 둘러보기</a>
-                </li>
-              </ul>
+      <div className={styles.mainContainer}>
+        <Header right={null} />
+        <div className={styles.container}>
+          <div className={styles.imgContainer}>
+            <Image
+              src="/assets/image.png"
+              width={800}
+              height={600}
+              alt="project"
+            />
+          </div>
+          <div className={styles.contentContainer}>
+            <h1>{project.title}</h1>
+            <h2>{project.subTitle}</h2>
+            <div>
+              <div className={styles.textContainer}>
+                <div className={styles.description}>{project.description}</div>
+                <ul>
+                  <li>
+                    <label>주요 기능</label>
+                    <div>{project.featuresDescription}</div>
+                  </li>
+                  <li>
+                    <label>역할</label>
+                    <div>{project.role}</div>
+                  </li>
+                  <li>
+                    <label>스택</label>
+                    <div>{project.stack}</div>
+                  </li>
+                  <li>
+                    <a href={project.projectRepository}>프로젝트 둘러보기</a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
--- client/components/about/index.tsx
+++ client/components/about/index.tsx
@@ -24,7 +24,7 @@ const About = ({
           <h1>Ahyeon, Jung</h1>
           <ul>
             <li key="first">사용자의 입장에서 즐거운 경험을 추구합니다</li>
-            <li key="second">서비스의 쓸모를 생각합니다</li>
+            <li key="second">서비스의 목적을 생각합니다</li>
             <li key="third">코드가 나아갈 방향을 고민합니다</li>
           </ul>
           <Contact />
@@ -38,11 +38,11 @@ const About = ({
                 <ul>
                   <h2>Education</h2>
                   <li key="elice">
-                    <span>2023.04 - 2023.11</span> Elice AI 트랙 8기
+                    <span>2023.04 - 2023.11</span> 엘리스 AI 트랙 8기 수료
                   </li>
                   <li key="univ">
-                    <span>2020.03 - 휴학중</span>경희대학교
-                    글로벌커뮤니케이션학부 입학
+                    <span>2020.03 - </span>경희대학교 글로벌커뮤니케이션학부
+                    입학
                   </li>
                   <li key="highschool">
                     <span>2017.03 - 2020.02</span>대원여자고등학교 졸업
@@ -56,36 +56,26 @@ const About = ({
                     <span>2023.01 - 2023.03</span>중소벤처기업진흥공단 사무보조
                   </li>
                   <li key="subway">
-                    <span>2022.07 - 2023.01</span>Subway Middle Manager
+                    <span>2022.07 - 2023.01</span>써브웨이 미들 매니저
                   </li>
                 </ul>
               </li>
               <li>
                 <ul>
-                  <h2>Award</h2>
+                  <h2>Activity</h2>
                   <li key="first">
-                    <span>2025.02</span>경희대학교 글로벌커뮤니케이션학부 졸업
+                    <span>2020.09 - 2022.12</span>교내 자치기구 편집부장 및
+                    영어기사 작성
                   </li>
                   <li key="second">
-                    <span>2020.02</span>대원여자고등학교 졸업
+                    <span>2017.05 - 2021.12</span>난민교육후원회 영어동화번역
+                    봉사단
                   </li>
                 </ul>
               </li>
               <li>
                 <ul>
                   <h2>Certification</h2>
-                  <li key="first">
-                    <span>2025.02</span>경희대학교 글로벌커뮤니케이션학부 졸업
-                  </li>
-                  <li key="second">
-                    <span>2020.02</span>대원여자고등학교 졸업
-                  </li>
-                  <li key="second">
-                    <span>2020.02</span>대원여자고등학교 졸업
-                  </li>
-                  <li key="second">
-                    <span>2020.02</span>대원여자고등학교 졸업
-                  </li>
                 </ul>
               </li>
             </ul>
